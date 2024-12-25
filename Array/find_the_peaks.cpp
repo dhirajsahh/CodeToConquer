d8fb8e733f2b0b1ef35ebeecacafb6031c74d920
dhirajsahh <dhirajmahato2019@gmail.com>
2024-12-25 21:08:42 +0545

    find_the_peaks

---
+++ Array/find_the_peaks.cpp
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/find-the-peaks/description/
+class Solution {
+public:
+    vector<int> findPeaks(vector<int>& mountain) {
+        int n=mountain.size();
+        vector<int>result;
+        for(int i=1;i<n-1;i++){
+           if(mountain[i]>mountain[i-1]&&mountain[i]>mountain[i+1]){
+            result.push_back(i);
+           }
+        }
+        return result;
+    }
+};
